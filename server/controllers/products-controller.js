ea4db955f55447dbbd1c97fd7da9db1fa10959a5
Kavitha <kavitha.pugal@gmail.com>
2023-02-05 02:58:34 +0530

    add logic for checkout

--- server/controllers/products-controller.js
+++ server/controllers/products-controller.js
@@ -19,14 +19,32 @@ exports.productsAll = async (req, res) => {
     })
 }
 
-// Create new cart
+// Retrieve all cart items
+exports.cartAll = async (req, res) => {
+  // Get all books from database'
+  console.log('Get cart items from database')
+  library
+    .select('*') // select all records
+    .from('cart') // from 'books' table
+    .then(userData => {
+      // Send books extracted from database in response
+      res.json(userData)
+      console.log('Retrieving all cart items')
+    })
+    .catch(err => {
+      // Send status code and error message in response
+      res.status(404).json({ message: `There was an error retrieving cart items: ${err}` })
+    })
+}
+
+// Add items to cart
 exports.addCart = async (req, res) => {
   console.log(">>>cart params:", req.body.id, " -- ", req.body.name)
   const customerid = 'C1'
   let quantity = 1
-  // let productExistsInCart
+
   // verify if the product already exists in the cart
-  library('cart')
+  library
     .select('*')
     .from('cart')
     .where('productid', req.body.id)
@@ -90,3 +108,84 @@ exports.addCart = async (req, res) => {
 }
 
 
+// Checkout from cart
+exports.checkout = async (req, res) => {
+  console.log(">>>cart params: Customer Id", req.body.id)
+  findTotalCost()
+  /*
+
+  - select all the items from the cart for the given customer id
+  - calculate the total cost - calculateTotalCost()
+  - if the order is nth order generate the discount code
+  - apply the discount on the total amount
+  - update the order table with the order details
+  - display the following on screen
+      -- Total # of Items
+      -- Total Amount
+      -- Discount Code
+      -- Discount Amount
+      -- Total Amount Payable
+  - Place another order (less important)
+      -- clear the cart
+      -- take control to Products component
+
+  */
+}
+
+exports.generateDiscountCode = () => {
+
+  /*
+  Generate the discount code, update in db and mark status as active
+  1. how to generate discount code? format?
+  2. global variable where to set?
+  */
+
+}
+
+exports.calculateTotalCost = () => {
+   /*
+    Return # of Items
+    Return Total Cost - Quantity * Price
+
+  */
+}
+
+exports.getOrderCount = () => {
+  /*
+   Return order count
+  */
+}
+
+findTotalCost = async () => {
+  // TODO should we match the Customerid?
+  console.log('>>> inside findTotalCost')
+  library('cart')
+    .join('products', 'cart.productid', '=', 'products.id')
+    .count ('cart.quantity', {as: 'count'})
+    //.select ('cart.quantity', 'products.price')
+    //.select(library.raw('(?? * ??) as amount', ['cart.quantity', 'products.price']))
+    //.sum(library.raw('(?? * ??)', ['cart.quantity', 'products.price']))
+    .sum({amount: library.raw('(?? * ??)', ['cart.quantity', 'products.price'])})
+    .then(userData => {
+      console.log('>>> cart count:', userData[0].count)
+      console.log('>>> total:', userData[0].amount)
+    })
+    .catch(err => {
+      console.log('>>>', err)
+      // Send status code and error message in response
+      //res.status(422).json({ message: `There was an error in getting cart details ${req.body.name}, Error: ${err}` })
+    })
+
+/*   library('cart')
+    .join('products', 'cart.productid', '=', 'products.id')
+    .count ('cart.quantity', {as: 'count'})
+    .then(userData => {
+      console.log('>>> cart count:', userData[0].count)
+    })
+    .catch(err => {
+      console.log('>>>', err)
+      // Send status code and error message in response
+      //res.status(422).json({ message: `There was an error in getting cart details ${req.body.name}, Error: ${err}` })
+    }) */
+  //return({count: count, total: total})  
+}
